02b7518f3f601a125de6c5dc7d87bf92d9e50b01
eFroD <eric.frodl@gmx.de>
2026-01-04 00:16:09 +0100

    cors settings

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ logfire.instrument_pydantic_ai()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://frontend:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
