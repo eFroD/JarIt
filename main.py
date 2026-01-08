7375aa6d47008417c56282c69078cf79257316bd
Eric <35428738+eFroD@users.noreply.github.com>
2026-01-08 23:36:12 +0100

    Feature/frontend (#29)
    * cors settings
    
    * cors settings
    
    * update gitignore
    
    * add frontend
    
    * add option to disable user creation
    
    * Add user registration setting
    
    * Add Frontend to deployment, fix user administration
    
    * cleaning
    
    * Rename the App
    
    * update deployment
    
    * add logo

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import logfire
 import os
-from recipe_agent.db.database import Base, engine
-from recipe_agent.api.router import router
+from jarit.db.database import Base, engine
+from jarit.api.router import router
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Recipe Agent API", version="1.0.0")
+app = FastAPI(title="JarIt API", version="1.0.0")
 
 logfire_token = os.environ.get("LOGFIRE_WRITE_TOKEN")
 logfire.configure(token=logfire_token)
 logfire.instrument_pydantic_ai()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.getenv("DOMAIN_NAME","http://localhost"),
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
 
 app.include_router(router)
